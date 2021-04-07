9fc9333725d1ae2f7cd96d86ee19b522d6742d22
Vanessa720 <vanessawilliamson718@gmail.com>
2021-04-06 23:57:48 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", function ( event ) {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  } );
